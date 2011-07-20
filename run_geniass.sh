f5c93d38daea851dd76927aa956ffa6c5c8326b5
Pontus Stenetorp <pontus@stenetorp.se>
2011-07-20 12:39:57 +0900

    Whining about the silliness of the wrapper script.

--- run_geniass.sh
+++ run_geniass.sh
@@ -4,6 +4,8 @@
 
 # TODO: Does not check for the existance of Ruby
 # TODO: Does not check for the existance of the binary
+# TODO: Path conversion etc. should be done by the program
+# XXX: Can't pass arguments to the program and doing so duplicates the logic
 
 if [ $# -ne 2 ] && [ $# -ne 3 ]; then
 	echo "Usage: run_geniass.sh <in-file> <out-file> [path-to-ruby]" 1>&2
